d4d3fa30bc9725ed66d06b55b9ce447243641c38
zenrizu <frederickmarco.fml@gmail.com>
2022-05-09 01:47:19 +0700

    form login

---
+++ kerjapraktik/FormLogin.Designer.cs
@@ -0,0 +1,160 @@
+﻿
+namespace kerjapraktik
+{
+    partial class FormLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.labelNama = new System.Windows.Forms.Label();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.textBoxUsername = new System.Windows.Forms.TextBox();
+            this.labelId = new System.Windows.Forms.Label();
+            this.labelWarna = new System.Windows.Forms.Label();
+            this.buttonKeluar = new System.Windows.Forms.Button();
+            this.buttonLogin = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.Controls.Add(this.labelNama);
+            this.panel1.Controls.Add(this.textBoxPassword);
+            this.panel1.Controls.Add(this.textBoxUsername);
+            this.panel1.Controls.Add(this.labelId);
+            this.panel1.Location = new System.Drawing.Point(-1, 52);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(444, 122);
+            this.panel1.TabIndex = 39;
+            // 
+            // labelNama
+            // 
+            this.labelNama.AutoSize = true;
+            this.labelNama.BackColor = System.Drawing.Color.Transparent;
+            this.labelNama.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelNama.Location = new System.Drawing.Point(19, 60);
+            this.labelNama.Name = "labelNama";
+            this.labelNama.Size = new System.Drawing.Size(91, 16);
+            this.labelNama.TabIndex = 26;
+            this.labelNama.Text = "PASSWORD :";
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.Location = new System.Drawing.Point(116, 59);
+            this.textBoxPassword.Multiline = true;
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.PasswordChar = '*';
+            this.textBoxPassword.Size = new System.Drawing.Size(279, 30);
+            this.textBoxPassword.TabIndex = 25;
+            // 
+            // textBoxUsername
+            // 
+            this.textBoxUsername.Location = new System.Drawing.Point(116, 25);
+            this.textBoxUsername.Multiline = true;
+            this.textBoxUsername.Name = "textBoxUsername";
+            this.textBoxUsername.Size = new System.Drawing.Size(279, 28);
+            this.textBoxUsername.TabIndex = 23;
+            // 
+            // labelId
+            // 
+            this.labelId.AutoSize = true;
+            this.labelId.BackColor = System.Drawing.Color.Transparent;
+            this.labelId.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelId.ForeColor = System.Drawing.Color.Black;
+            this.labelId.Location = new System.Drawing.Point(25, 26);
+            this.labelId.Name = "labelId";
+            this.labelId.Size = new System.Drawing.Size(85, 16);
+            this.labelId.TabIndex = 22;
+            this.labelId.Text = "USERNAME :";
+            // 
+            // labelWarna
+            // 
+            this.labelWarna.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.labelWarna.Font = new System.Drawing.Font("Tahoma", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelWarna.ForeColor = System.Drawing.Color.White;
+            this.labelWarna.Location = new System.Drawing.Point(-1, -1);
+            this.labelWarna.Name = "labelWarna";
+            this.labelWarna.Size = new System.Drawing.Size(444, 41);
+            this.labelWarna.TabIndex = 42;
+            this.labelWarna.Text = "LOGIN";
+            this.labelWarna.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // buttonKeluar
+            // 
+            this.buttonKeluar.BackColor = System.Drawing.Color.Red;
+            this.buttonKeluar.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonKeluar.ForeColor = System.Drawing.Color.White;
+            this.buttonKeluar.Location = new System.Drawing.Point(319, 181);
+            this.buttonKeluar.Name = "buttonKeluar";
+            this.buttonKeluar.Size = new System.Drawing.Size(111, 41);
+            this.buttonKeluar.TabIndex = 41;
+            this.buttonKeluar.Text = "KELUAR";
+            this.buttonKeluar.UseVisualStyleBackColor = false;
+            // 
+            // buttonLogin
+            // 
+            this.buttonLogin.BackColor = System.Drawing.Color.White;
+            this.buttonLogin.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonLogin.ForeColor = System.Drawing.Color.Red;
+            this.buttonLogin.Location = new System.Drawing.Point(196, 181);
+            this.buttonLogin.Name = "buttonLogin";
+            this.buttonLogin.Size = new System.Drawing.Size(117, 41);
+            this.buttonLogin.TabIndex = 40;
+            this.buttonLogin.Text = "LOGIN";
+            this.buttonLogin.UseVisualStyleBackColor = false;
+            // 
+            // FormLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(442, 234);
+            this.ControlBox = false;
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.labelWarna);
+            this.Controls.Add(this.buttonKeluar);
+            this.Controls.Add(this.buttonLogin);
+            this.Name = "FormLogin";
+            this.Text = "Form Login";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label labelNama;
+        private System.Windows.Forms.TextBox textBoxPassword;
+        private System.Windows.Forms.TextBox textBoxUsername;
+        private System.Windows.Forms.Label labelId;
+        private System.Windows.Forms.Label labelWarna;
+        private System.Windows.Forms.Button buttonKeluar;
+        private System.Windows.Forms.Button buttonLogin;
+    }
+}
